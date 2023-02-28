590f9abd0d6e0b59d62818db7487234107a5c510
Ilie Laurentiu <ilielaurentiu8@yahoo.com>
2023-02-28 18:30:04 +0200

    First version of UI
    Implemented the first working version of UI text

--- Assets/Scripts/Application.py
+++ Assets/Scripts/Application.py
@@ -1,9 +1,36 @@
 import pygame
-from . import GameWindow
+from Assets.Scripts.UI.Menu import * 
+
 
 def Run():
     pygame.init()
-    GameWindow.Open()
+    pygame.display.set_caption('Pong')
+    OpenWindow()
+
+def OpenWindow():
+
+    BG_COLOR = (40, 40, 40)
+    SCREEN_WIDTH = 900
+    SCREEN_HEIGHT = 900
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    gameRunning = True
+    # event handler
+    while gameRunning:
+        for event in pygame.event.get():    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print("Space pressed")
+                
+            if event.type == pygame.QUIT:
+                gameRunning = False
+
+            screen.fill(BG_COLOR)
+            draw_text("Press SPACE to write to console", font, TEXT_COLOR, 200, 450)
+
+            pygame.display.flip()
+            pygame.display.update()
+        
 
 def Quit():
     pygame.quit()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Assets.Scripts import Application
 
+# run the application
 def main():
-   Application.Run()
+   Application.Run() 
 
+# check if only one instance of the app (main function) is running
 if __name__ == "__main__":
     main()
